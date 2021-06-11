2ad5e5e8b17de62ff5522838166c44fd868d4632
Allymonies <github@periodic.xyz>
2021-06-11 05:17:09 +0200

    feat: GET /staking lists stakes

---
+++ src/controllers/staking.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Drew Lemmy, 2016-2021
+ *
+ * This file is part of Tenebra.
+ *
+ * Tenebra is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tenebra is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tenebra. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more project information, see <https://github.com/allymonies/tenebra>.
+ */
+
+const staking      = require("./../staking.js");
+const addresses    = require("./../addresses.js");
+const tenebra      = require("./../tenebra.js");
+const errors       = require("./../errors/errors.js");
+
+function StakingController() {}
+
+StakingController.getStakes = function (limit, offset, asc, includeInactive) {
+  return new Promise(function(resolve, reject) {
+    if ((limit && isNaN(limit)) || (limit && limit <= 0)) {
+      return reject(new errors.ErrorInvalidParameter("limit"));
+    }
+
+    if ((offset && isNaN(offset)) || (offset && offset < 0)) {
+      return reject(new errors.ErrorInvalidParameter("offset"));
+    }
+
+    staking.getStakes(limit, offset, asc, includeMined).then(resolve).catch(reject);
+  });
+};
+
+StakingController.stakeToJSON = function(stake) {
+  return staking.stakeToJSON(stake);
+};
+
+module.exports = StakingController;
---
+++ src/staking.js
@@ -0,0 +1,65 @@
+/**
+ * Created by Drew Lemmy, 2016-2021
+ *
+ * This file is part of Tenebra.
+ *
+ * Tenebra is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tenebra is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tenebra. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more project information, see <https://github.com/allymonies/tenebra>.
+ */
+
+const utils        = require("./utils.js");
+const schemas      = require("./schemas.js");
+const websockets   = require("./websockets.js");
+const addresses    = require("./addresses.js");
+const { Op }       = require("sequelize");
+const escapeRegExp = require("lodash.escaperegexp");
+
+const promClient = require("prom-client");
+const promStakesCounter = new promClient.Counter({
+  name: "tenebra_stakes_total",
+  help: "Total number of stakes."
+});
+
+// Query operator to exclude stakes that aren't active
+const EXCLUDE_INACTIVE = {
+  [Op.eq]: 1, // Active field is true
+  [Op.is]: null // Or active field is not set
+};
+
+function Staking() {}
+
+/*Transactions.getTransaction = function(id) {
+  return schemas.transaction.findByPk(id);
+};*/
+
+Staking.getStakes = function (limit, offset, asc, includeInactive) {
+  return schemas.stake.findAndCountAll({
+    order: [["id", asc ? "ASC" : "DESC"]],
+    limit: utils.sanitiseLimit(limit),
+    offset: utils.sanitiseOffset(offset),
+    where: includeInactive ? {} : { from: EXCLUDE_INACTIVE }
+  });
+};
+
+
+Staking.stakeToJSON = function(stake) {
+  return {
+    owner: stake.owner,
+    amount: stake.amount,
+    active: stake.active == 1
+  };
+};
+
+module.exports = Staking;
