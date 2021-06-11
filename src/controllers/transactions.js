d4292b2f37b1ccf6d715ea2ce6ce33487ee0f526
Allymonies <github@periodic.xyz>
2021-06-11 04:25:41 +0200

    chore: Fix github links in license

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Tenebra. If not, see <http://www.gnu.org/licenses/>.
  *
- * For more project information, see <https://github.com/tmpim/tenebra>.
+ * For more project information, see <https://github.com/allymonies/tenebra>.
  */
 
 const transactions = require("./../transactions.js");
--- src/routes/zz_homepage.js
+++ src/routes/zz_homepage.js
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Tenebra. If not, see <http://www.gnu.org/licenses/>.
  *
- * For more project information, see <https://github.com/tmpim/tenebra>.
+ * For more project information, see <https://github.com/allymonies/tenebra>.
  */
 
 // File is named zz_homepage to ensure it runs after all the legacy API routes
